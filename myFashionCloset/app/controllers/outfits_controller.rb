9df0353013f1d3692768209a81513a46af8737f5
Mattia Russo <tia@DESKTOP-2L738B4.localdomain>
2022-08-31 16:32:24 +0200

    Aggiunta la show degli outfit in dettaglio

--- myFashionCloset/app/controllers/outfits_controller.rb
+++ myFashionCloset/app/controllers/outfits_controller.rb
@@ -47,6 +47,19 @@ class OutfitsController < ApplicationController
         end
     end
 
+
+    def show
+        begin
+            @outfit = Outfit.find(params[:id])
+            @creator = Creator.find(@outfit.creator_id)
+            @user = User.find(@creator.user_id)
+            @capos = @outfit.capos;
+        rescue =>e
+            flash[:alert] = "Searched outfit doesn't exist"
+            redirect_to root_path
+        end
+    end
+
     #Endpoint to search capos without page reload
     def search
         @q = Capo.ransack(params)
