e9c941f95e65b0b1ef18e3355bd397c0a9c092d1
Mattia Russo <tia@DESKTOP-2L738B4.localdomain>
2022-09-08 16:07:04 +0200

    Aggiunto bootstrap alert

--- myFashionCloset/app/helpers/application_helper.rb
+++ myFashionCloset/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def flash_class(level)              #Used to get bootstrap alert class from flash alert class
+        bootstrap_alert_class = {
+          "success" => "alert-success",
+          "error" => "alert-danger",
+          "notice" => "alert-info",
+          "alert" => "alert-danger",
+          "warn" => "alert-warning"
+        }
+        bootstrap_alert_class[level]
+      end
+
 end
