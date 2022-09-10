2b19f69ec9e2ae47a3240334d1a8f2cd91c0373b
Mattia Russo <tia@DESKTOP-2L738B4.localdomain>
2022-09-10 17:44:54 +0200

    Request http gestite con thread

--- myFashionCloset/app/services/articolo.rb
+++ myFashionCloset/app/services/articolo.rb
@@ -8,215 +8,282 @@ class Articolo
 
     def self.save_top_popular_articles
 
+        capos = []
+        semaphore = Mutex.new
+
         #woman main Forever 21
-        begin
-            response = HTTP.timeout(10).get("https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&category=woman_main&pageSize=15&pageNumber=1&sortby=0")
-        rescue => e
-            return false
-        end
-        results = response.parse['CatalogProducts']
-
-        results.each do |r|
-            if Capo.where("nome = ?", r['DisplayName']).group(:nome).count == {}
-                c = Capo.new
-                c.sito = 'Forever 21'
-                c.categoria = r['CategoryName']
-                c.immagine = r['DefaultProductImage']
-                c.nome = r['DisplayName']
-                c.descrizione = r['ProductShareLinkUrl']
-                c.save!
+        t1 = Thread.new{
+            begin
+                response = HTTP.timeout(10).get("https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&category=woman_main&pageSize=15&pageNumber=1&sortby=0")
+            rescue => e
+                return false
             end
-        end
+            results = response.parse['CatalogProducts']
 
-        #nuovi Arrivi Forever 21
-        begin
-            response = HTTP.timeout(10).get("https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&category=women_new_arrivals_clothing&pageSize=15&pageNumber=2&sortby=0")
-        rescue => e
-            return false
-        end
-        results = response.parse['CatalogProducts']
-
-        results.each do |r|
-            if Capo.where("nome = ?", r['DisplayName']).group(:nome).count == {}
-                c = Capo.new
-                c.sito = 'Forever 21'
-                #c.categoria = r['CategoryName']
-                c.categoria = 'New in'
-                c.immagine = r['DefaultProductImage']
-                c.nome = r['DisplayName']
-                c.descrizione = r['ProductShareLinkUrl']
-                c.save!
+            results.each do |r|
+                if Capo.where("nome = ?", r['DisplayName']).group(:nome).count == {}
+                    c = Capo.new
+                    c.sito = 'Forever 21'
+                    c.categoria = r['CategoryName']
+                    c.immagine = r['DefaultProductImage']
+                    c.nome = r['DisplayName']
+                    c.descrizione = r['ProductShareLinkUrl']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
             end
-        end
+        
+
+            #nuovi Arrivi Forever 21
+        
+            begin
+                response = HTTP.timeout(10).get("https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&category=women_new_arrivals_clothing&pageSize=15&pageNumber=2&sortby=0")
+            rescue => e
+                return false
+            end
+            results = response.parse['CatalogProducts']
+
+            results.each do |r|
+                if Capo.where("nome = ?", r['DisplayName']).group(:nome).count == {}
+                    c = Capo.new
+                    c.sito = 'Forever 21'
+                    #c.categoria = r['CategoryName']
+                    c.categoria = 'New in'
+                    c.immagine = r['DefaultProductImage']
+                    c.nome = r['DisplayName']
+                    c.descrizione = r['ProductShareLinkUrl']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end
+        }
 
         #Nuovi Arrivi Asos
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=2623&categoryId=25423&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
-
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = 'New in'
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+        t2 = Thread.new{
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=2623&categoryId=25423&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
             end
-        end 
+            results = response.parse['products']
+            categ = response.parse['categoryName']
 
-        #Accessori Asos
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=25423&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = 'New in'
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end 
+        
 
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+
+            #Accessori Asos
+     
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=25423&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
             end
-        end 
+            results = response.parse['products']
+            categ = response.parse['categoryName']
+
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end 
+        }
+
 
         #Pants e leggins Asos
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=27420&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
-
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+        t3 = Thread.new{
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=27420&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
             end
-        end 
+            results = response.parse['products']
+            categ = response.parse['categoryName']
 
-        #Shoes ASOS
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=27416&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end 
+        
 
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+            #Shoes ASOS
+        
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=27416&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
             end
-        end
+            results = response.parse['products']
+            categ = response.parse['categoryName']
+
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end
+        }
+
 
         #Sweaters e cardigans ASOS
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=27409&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
-
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+        t4 = Thread.new{
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=27409&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
             end
-        end
+            results = response.parse['products']
+            categ = response.parse['categoryName']
 
-        #Jeans ASOS
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=27405&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
-
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
             end
-        end
+        
+
+
+            #Jeans ASOS
+        
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=27405&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
+            end
+            results = response.parse['products']
+            categ = response.parse['categoryName']
+
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end
+        }
 
         #Hoodies & Sweatshirts ASOS
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=27423&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
-
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+        t5 = Thread.new{
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=27423&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
             end
-        end
+            results = response.parse['products']
+            categ = response.parse['categoryName']
 
-        #Dresses ASOS
-        begin
-            response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=10e9f78b6cmsh4413dcb186b3dbdp14a5f3jsn76f3e31bc2b1&store=US&offset=0&categoryId=27399&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
-        rescue => e
-            return false
-        end
-        results = response.parse['products']
-        categ = response.parse['categoryName']
-
-        results.each do |r|
-            if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
-                c = Capo.new
-                c.sito = 'Asos'
-                c.categoria = categ
-                c.immagine = 'https://'+r['imageUrl']
-                c.nome = r['name']
-                c.descrizione = 'https://asos.com/'+r['url']
-                c.save!
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
             end
-        end
+        
 
+            #Dresses ASOS
+        
+            begin
+                response = HTTP.timeout(10).get("https://asos2.p.rapidapi.com/products/v2/list?rapidapi-key=35a27c5c76mshb714784ccca50e1p1bb29bjsncfebb6da8fd0&store=US&offset=0&categoryId=27399&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US")
+            rescue => e
+                return false
+            end
+            results = response.parse['products']
+            categ = response.parse['categoryName']
+
+            results.each do |r|
+                if Capo.where('nome = ?', r['name']).group(:nome).count == {} 
+                    c = Capo.new
+                    c.sito = 'Asos'
+                    c.categoria = categ
+                    c.immagine = 'https://'+r['imageUrl']
+                    c.nome = r['name']
+                    c.descrizione = 'https://asos.com/'+r['url']
+                    semaphore.lock
+                    capos.push(c)
+                    semaphore.unlock
+                    #c.save!
+                end
+            end
+        }
+
+        t1.join
+        t2.join
+        t3.join
+        t4.join
+        t5.join
+
+        capos.each do |c|
+            c.save!
+        end
+        
         return true
     end
 end
