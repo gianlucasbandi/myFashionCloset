7969e800ca5782dfb5b7f739dca55a4d99c6e95b
Mattia Russo <tia@DESKTOP-2L738B4.localdomain>
2022-09-14 20:18:49 +0200

    iniziato rspec controller outfit

---
+++ myFashionCloset/spec/controllers/outfits_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe OutfitsController, type: :controller do
+    
+    describe "GET #new" do
+        context "with unlogged user" do
+            it "returns to login page" do
+                get :new
+                expect(response).to redirect_to(new_user_session_path)
+                expect(flash[:alert]).to be_present
+            end
+        end
+
+        context "with logged not creator user" do
+            it "returns to home page" do
+                user = User.create(
+                   email: 'user@gmail.com',
+                   password: 'password1234',
+                   username: 'user',
+                   creat: nil)
+
+                login_as(user, :scope => :user)
+                get :new
+                expect(response).to redirect_to(root_path)  #Viene riportata all user_sign
+                expect(flash[:alert]).to be_present
+            end
+        end
+        
+    end
+    
+
+end
